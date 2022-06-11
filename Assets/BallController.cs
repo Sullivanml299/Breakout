d1da9baeb21315e031fb01b6570b67e4c255866d
Marcus <70675302+Sullivanml299@users.noreply.github.com>
2022-06-11 16:55:02 -0400

    better ball movement

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public float speed;
+    public float maxSpeed;
+    public float minSpeed;
+    public float minYSpeed;
     public GameObject paddle;
+    public float paddleLength;
+
     private Rigidbody2D thisRigidBody;
     private Vector3 lastVelocity;
+    private int blockLayer;
+    private float currentSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         thisRigidBody = GetComponent<Rigidbody2D>();
-        thisRigidBody.velocity = new Vector3(1,1, 0);
+        thisRigidBody.velocity = new Vector3(1,1, 0)*minSpeed;
+        blockLayer = LayerMask.NameToLayer("Blocks");
+        paddleLength = paddle.transform.localScale.x/2;
+        currentSpeed = minSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
         var v = thisRigidBody.velocity.normalized;
-        v = v*speed;
+        v = v*currentSpeed;
         thisRigidBody.velocity = v;
         lastVelocity = v;
     }
@@ -30,14 +40,29 @@ public class BallController : MonoBehaviour
         Vector3 newDirection;
 
         if(col.gameObject == paddle){
-            paddleAdjustment = transform.position.x - paddle.transform.position.x;
+            paddleAdjustment = scaleDirection(transform.position.x - paddle.transform.position.x);
             newDirection = Vector3.Reflect(lastVelocity.normalized, col.contacts[0].normal);
             newDirection = new Vector3(paddleAdjustment, newDirection.y, newDirection.z);
+            currentSpeed = Mathf.Max(maxSpeed * Mathf.Abs(paddleAdjustment), minSpeed);
+            var newVelocity = newDirection * currentSpeed;
+            newVelocity = new Vector3(newVelocity.x, Mathf.Max(newVelocity.y, minYSpeed), newVelocity.z);
+            thisRigidBody.velocity = newVelocity;
+            print(thisRigidBody.velocity);
         }
-        else{
+        else
+        {
             newDirection = Vector3.Reflect(lastVelocity.normalized, col.contacts[0].normal);
+            thisRigidBody.velocity = newDirection * currentSpeed;
         }
-        thisRigidBody.velocity = newDirection*speed;
+        
+        if(col.gameObject.layer == blockLayer)
+        {
+            Destroy(col.gameObject);
+        }
+    }
 
+    private float scaleDirection(float direction)
+    {
+        return direction / paddleLength;
     }
 }
